7113ebecc4261b13e8a7faae08d57de53fb2dc06
ashnaqburst <ashna@qburst.com>
2018-07-17 11:57:06 +0530

    created the html view of login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import './Login.css';
+import styles from './Login.css';
 
 class Login extends React.Component{
     render() {
         return(
-            <div className="login-container">
-
+            <div className={styles.loginContainer}>
+                <h1>Login</h1>
+                <p>Username</p>
+                <input type="text" placeholder="Username"/>
+                <p>Password</p>
+                <input type="password" name="" id="" placeholder="Password"/>
+                <button> Login </button>
+                <div>
+                     <a href=""> Register </a>
+                </div>     
             </div>
             
         );
