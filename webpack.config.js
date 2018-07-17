09274de1c7c1e4f73235de19c57fa3aa6b8a50a5
shinyrajrl <shinyr@qburst.com>
2018-07-17 10:22:11 +0530

    login component and configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
     module: {
       rules: [
         {
           test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.jsx?$/,
           exclude: /node_modules/,
           use: {
             loader: "babel-loader"
@@ -27,7 +40,7 @@ module.exports = {
               }
             }
           ]
-        }  
+        }
       ]
     },
     plugins: [
